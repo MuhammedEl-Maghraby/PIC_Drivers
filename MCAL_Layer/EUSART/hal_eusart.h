97b8a4cfaa4fc8c9f3724cbde338861df60925df
xoxoxo <muhammedelmaghraby73@gmail.com>
2022-07-31 22:40:52 +0200

    added EUSART to the MCAL Layer

---
+++ MCAL_Layer/EUSART/hal_eusart.h
@@ -0,0 +1,32 @@
+/* 
+ * File:   hal_eusart.h
+ * Author: Maghraby
+ *
+ * Created on July 31, 2022, 10:30 PM
+ */
+
+#ifndef HAL_EUSART_H
+#define	HAL_EUSART_H
+
+/* Section: Includes*/
+#include "../../MCAL_Layer/Interrupt/mcal_internal_interrupt.h"
+#include "../proc/pic18f4620.h"
+#include "../mcal_std_types.h"
+
+
+/*Section: Macro Definitions*/
+
+
+/*Section: Function Like Macros*/
+
+
+/*Section: Data Type Declaration*/
+
+
+
+/*Section: Function Prototypes*/
+
+
+
+#endif	/* HAL_EUSART_H */
+
---
+++ MCAL_Layer/EUSART/hal_eusart_cfg.h
@@ -0,0 +1,13 @@
+/* 
+ * File:   hal_eusart_cfg.h
+ * Author: Maghraby
+ *
+ * Created on July 31, 2022, 10:30 PM
+ */
+
+#ifndef HAL_EUSART_CFG_H
+#define	HAL_EUSART_CFG_H
+
+
+#endif	/* HAL_EUSART_CFG_H */
+
